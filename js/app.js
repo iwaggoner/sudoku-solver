c046164bcb8e12e95a10b3d4f89512023c7c45c7
iwaggoner <iwaggoner95@gmail.com>
2021-10-26 11:53:11 -0400

    Got function getUsedNums working for 9 by 9 board and will start working on my generate number function based my used nums

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// take in Array for 4x4
+// take in Array for 9x9
 // begin to fill in numbers for first missing array elements
 // generate random number after checking numbers in rows and columns as well as box to narrow down possiblities
 // test rows, columns, and boxes
@@ -38,7 +38,7 @@ form.addEventListener('submit', (e)=> {
         console.log('looking for empty cell')  
         for (let i = 0;i<board[0].length;i++) {
             for(let j = 0;j<board[0].length;j++){
-                if(board[i][j] == '' || board[i][j] == 0){
+                if(board[i][j] == 0){
                     console.log('found empty cell')
                     currentPos.push(i)
                     currentPos.push(j)
@@ -51,23 +51,41 @@ form.addEventListener('submit', (e)=> {
 
     // function that narrows down what numbers can fit in the current spot
     // used to speed up program
+    // will use very similar function to check if numbers are correct
     const getUsedNums = (board, currentPos) => {
         console.log('getting Used Nums')
 
-            var rowNums = board[currentRow].filter(isNotEmpty)
+            // save all non 0 numbers in the same row as empty cell to an array
+            var rowNums = board[currentPos[0]].filter(isNotEmpty)
             rowNums.forEach(rowNums => takenNums.push(rowNums))
+            console.log(takenNums)
 
-            var colNums = cols[currentCol].filter(isNotEmpty)
-            colNums.forEach(colNums => takenNums.push(colNums))
+            // save all non 0 numbers in the same col as empty cell to any array
+            for(let i = 0;i<board[0].length;i++){
+                if(board[i][currentPos[1]] != 0){
+                    takenNums.push(board[i][currentPos[1]])
+                }
+            }
+            console.log(takenNums)
 
-            // get numbers in same box
-
-
-            
+            // get numbers in same box based on empty cell
+            // can divide each position by 3 and then round down to find starting point of box
+            var startX = Math.floor(currentPos[0]/3)
+            var startY = Math.floor(currentPos[1]/3)
+            // set up double loop to collect each non 0 element in postion box
+            for(let i = 0;i<startX+3;i++){
+                for(let j = 0;j<startY+3;j++){
+                    if(board[i][j] != 0){
+                        takenNums.push(board[i][j])
+                    } 
+                }
+            }
+            console.log(takenNums)
     }
 
+    // used equation to filter out all values that are not 0
     function isNotEmpty(value) {
-        return value !== ''
+        return value !== 0
     }
 
     const getNewNum = () => {
@@ -101,6 +119,8 @@ form.addEventListener('submit', (e)=> {
 
     lookForEmptyCell(board)
     console.log(currentPos)
+    getUsedNums(board, currentPos)
+
     
 
     document.getElementById('solved').style.display = 'flex'
