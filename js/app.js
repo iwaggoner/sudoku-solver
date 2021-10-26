f2f24bda784f65722de4e849f56ed0e7938e16d0
iwaggoner <iwaggoner95@gmail.com>
2021-10-26 15:01:17 -0400

    Found that function getUsedNums is not a good funciton and that I should instead of saveing used numbers from Col, Rows, and 3 by 3 box, I should just access these in the funciton and test if board is still correct

--- js/app.js
+++ js/app.js
@@ -49,86 +49,109 @@ form.addEventListener('submit', (e)=> {
     }
 
 
-    // function that narrows down what numbers can fit in the current spot
-    // used to speed up program
-    // will use very similar function to check if numbers are correct
-    const getUsedNums = (board, currentPos) => {
-        console.log('getting Used Nums')
+    // // function that narrows down what numbers can fit in the current spot
+    // // used to speed up program
+    // // will use very similar function to check if numbers are correct
+    // const getUsedNums = (board, currentPos) => {
+    //     console.log('getting Used Nums')
 
-            // save all non 0 numbers in the same row as empty cell to an array
-            var rowNums = board[currentPos[0]].filter(isNotEmpty)
-            rowNums.forEach(rowNums => takenNums.push(rowNums))
-            console.log(takenNums)
+    //         // save all non 0 numbers in the same row as empty cell to an array
+    //         var rowNums = board[currentPos[0]].filter(isNotEmpty)
+    //         rowNums.forEach(rowNums => takenNums.push(rowNums))
+    //         console.log(takenNums)
 
-            // save all non 0 numbers in the same col as empty cell to any array
-            for(let i = 0;i<board[0].length;i++){
-                if(board[i][currentPos[1]] != 0){
-                    takenNums.push(board[i][currentPos[1]])
-                }
+    //         // save all non 0 numbers in the same col as empty cell to any array
+    //         for(let i = 0;i<board[0].length;i++){
+    //             if(board[i][currentPos[1]] != 0){
+    //                 takenNums.push(board[i][currentPos[1]])
+    //             }
+    //         }
+    //         console.log(takenNums)
+
+    //         // get numbers in same box based on empty cell
+    //         // can divide each position by 3 and then round down to find starting point of box
+    //         var startX = Math.floor(currentPos[0]/3)
+    //         var startY = Math.floor(currentPos[1]/3)
+    //         // set up double loop to collect each non 0 element in postion box
+    //         for(let i = 0;i<startX+3;i++){
+    //             for(let j = 0;j<startY+3;j++){
+    //                 if(board[i][j] != 0){
+    //                     takenNums.push(board[i][j])
+    //                 } 
+    //             }
+    //         }
+    //         console.log(takenNums)
+    // }
+
+    // almost the same function as getUsedNums but check if number generated is good num
+    // has to pass num, board, and current position
+    // if good it returns true and num is entered into board
+    const correctBoard = (board, num, currentPos) => {
+
+        // checks all rows
+        for(let i = 0;i<board[0].length;i++){
+            if(board[i][currentPos[1]] == num){
+                return false
             }
-            console.log(takenNums)
-
-            // get numbers in same box based on empty cell
-            // can divide each position by 3 and then round down to find starting point of box
-            var startX = Math.floor(currentPos[0]/3)
-            var startY = Math.floor(currentPos[1]/3)
+        }
+        // checks all cols
+        for(let i = 0;i<board[0].length;i++){
+            if(board[i][currentPos[1]] == num){
+                return false
+            }
+        }
+        var startX = Math.floor(currentPos[0]/3)
+        var startY = Math.floor(currentPos[1]/3)
             // set up double loop to collect each non 0 element in postion box
             for(let i = 0;i<startX+3;i++){
                 for(let j = 0;j<startY+3;j++){
-                    if(board[i][j] != 0){
-                        takenNums.push(board[i][j])
+                    if(board[i][j] == num){
+                        return false
                     } 
                 }
             }
-            console.log(takenNums)
+        return true
     }
-
     // used equation to filter out all values that are not 0
     function isNotEmpty(value) {
         return value !== 0
     }
 
+    // used array takenNums to narrow down what the program can select and test if okay number to use
     const getNewNum = (board, pos) => {
         console.log('getting new num')
-        console.log(takenNums)
-                for(let i = 1;i<10;i++){
-                if(takenNums.includes(`${i}`)){
-                } else {
-                    console.log('adding num to array')
+            for(let i = 1;i<10;i++){
+                if(correctBoard(board, i, pos)){
+                    console.log('adding num to array after testing if correct')
                     console.log(i)
                     board[pos[0]][pos[1]] = i
                     break
                 }
-            } 
-    }
+                board[pos[0]][pos[1]] = 0
+            }
+    } 
+    
 
-    // const solveBoard = () => {
-    //         while(boradSolved){
-    //             lookForEmptyCell()
-    //             getUsedNums()
-    //             getNewNum()
-    //             takenNums = []
-    //             console.log(rows)
-    //             console.log(cols)
-    //     }
-    // }
 
     const test = (board) => {
 
-    lookForEmptyCell(board)
-    console.log(currentPos)
-    getUsedNums(board, currentPos)
-    getNewNum(board,currentPos)
-    console.log(board)
+        for(let i = 0;i<10;i++){
+            lookForEmptyCell(board)
+            console.log(currentPos)
+            getNewNum(board,currentPos)
+            console.log(board)
+            takenNums = []
+            currentPos = []
+        }
     }
 
     // print out solved board
     // also help debug
+    // is much better as for loop rather than 81 lines of code
     const setBoard = (board) => {
         var inputSelector = 1
         for(let i = 0;i<board[0].length;i++){
             for(let j = 0;j<board[0].length;j++){
-                console.log(inputSelector,i,j,board[i][j])
                 document.getElementById(`solved${inputSelector}`).textContent = board[i][j]
                 inputSelector += 1
             }
@@ -137,11 +160,6 @@ form.addEventListener('submit', (e)=> {
     
     test(board)
     setBoard(board)
-    
-    
-   
-
-    
 
     document.getElementById('solved').style.display = 'flex'
     document.getElementById('solvedBoard').style.display = 'flex'
