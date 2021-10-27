280cfb0e3d1e25133ba64fe7dc10b4fff2c46ba2
iwaggoner <iwaggoner95@gmail.com>
2021-10-27 09:29:29 -0400

    My program is now using recursion, I am calling getNewNum in my test function and then calling test again in my getNewNum, but somewhere my testing is incorrect and I am getting all falses and the program enters all zeros in possible positions

--- js/app.js
+++ js/app.js
@@ -42,61 +42,35 @@ form.addEventListener('submit', (e)=> {
                     console.log('found empty cell')
                     currentPos.push(i)
                     currentPos.push(j)
+                    console.log(currentPos)
                     return
                 }
             }
         }
+        return true
     }
 
 
-    // // function that narrows down what numbers can fit in the current spot
-    // // used to speed up program
-    // // will use very similar function to check if numbers are correct
-    // const getUsedNums = (board, currentPos) => {
-    //     console.log('getting Used Nums')
 
-    //         // save all non 0 numbers in the same row as empty cell to an array
-    //         var rowNums = board[currentPos[0]].filter(isNotEmpty)
-    //         rowNums.forEach(rowNums => takenNums.push(rowNums))
-    //         console.log(takenNums)
-
-    //         // save all non 0 numbers in the same col as empty cell to any array
-    //         for(let i = 0;i<board[0].length;i++){
-    //             if(board[i][currentPos[1]] != 0){
-    //                 takenNums.push(board[i][currentPos[1]])
-    //             }
-    //         }
-    //         console.log(takenNums)
-
-    //         // get numbers in same box based on empty cell
-    //         // can divide each position by 3 and then round down to find starting point of box
-    //         var startX = Math.floor(currentPos[0]/3)
-    //         var startY = Math.floor(currentPos[1]/3)
-    //         // set up double loop to collect each non 0 element in postion box
-    //         for(let i = 0;i<startX+3;i++){
-    //             for(let j = 0;j<startY+3;j++){
-    //                 if(board[i][j] != 0){
-    //                     takenNums.push(board[i][j])
-    //                 } 
-    //             }
-    //         }
-    //         console.log(takenNums)
-    // }
-
-    // almost the same function as getUsedNums but check if number generated is good num
-    // has to pass num, board, and current position
-    // if good it returns true and num is entered into board
+    // has to pass num from getNewNum, board, and current position
+    // then test the row position gives
+    // then tests col position is in
+    // then test box based on postion
+    // if passes all test returns true
+    // if fails any test returns false
     const correctBoard = (board, num, currentPos) => {
 
         // checks all rows
         for(let i = 0;i<board[0].length;i++){
-            if(board[i][currentPos[1]] == num){
+            if(board[currentPos[0]][i] == num){
+                console.log('found false in row')
                 return false
             }
         }
         // checks all cols
         for(let i = 0;i<board[0].length;i++){
             if(board[i][currentPos[1]] == num){
+                console.log('found false in col')
                 return false
             }
         }
@@ -106,43 +80,51 @@ form.addEventListener('submit', (e)=> {
             for(let i = 0;i<startX+3;i++){
                 for(let j = 0;j<startY+3;j++){
                     if(board[i][j] == num){
+                        console.log('found false in box')
                         return false
                     } 
                 }
             }
         return true
     }
+
     // used equation to filter out all values that are not 0
     function isNotEmpty(value) {
         return value !== 0
     }
 
-    // used array takenNums to narrow down what the program can select and test if okay number to use
+    // takes in board and postion, then trys to inscert numbers 1-9 into position
+    // once correct board comes back true it sets that number into that location and breaks loop ends function
     const getNewNum = (board, pos) => {
         console.log('getting new num')
             for(let i = 1;i<10;i++){
                 if(correctBoard(board, i, pos)){
-                    console.log('adding num to array after testing if correct')
+                    console.log('adding num to array after test for correct was true')
                     console.log(i)
                     board[pos[0]][pos[1]] = i
-                    break
+                    if(test(board)) {
+                        console.log('running test again')
+                        return true
+                    }
                 }
-                board[pos[0]][pos[1]] = 0
+                else {
+                    board[pos[0]][pos[1]] = 0
+                    console.log('correctBoard was false for', i)
+                    console.log('setting',pos,'to 0')
+                }
             }
+            
     } 
     
 
 
     const test = (board) => {
-
-        for(let i = 0;i<10;i++){
-            lookForEmptyCell(board)
-            console.log(currentPos)
-            getNewNum(board,currentPos)
-            console.log(board)
-            takenNums = []
-            currentPos = []
+        currentPos = []
+        if(lookForEmptyCell(board)){
+            console.log('look for empty cell came back true, Board is solved')
+            return true
         }
+        getNewNum(board,currentPos)
     }
 
     // print out solved board
